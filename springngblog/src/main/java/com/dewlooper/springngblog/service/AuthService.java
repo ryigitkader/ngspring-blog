6c1eba71268f6100f8e3bb38126911f2124e9b74
dreamer <dreamer@dream.local>
2020-05-24 00:36:28 +0300

    Encode password with springSecurity

--- springngblog/src/main/java/com/dewlooper/springngblog/service/AuthService.java
+++ springngblog/src/main/java/com/dewlooper/springngblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.dewlooper.springngblog.dto.RegisterRequest;
 import com.dewlooper.springngblog.model.User;
 import com.dewlooper.springngblog.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +13,22 @@ public class AuthService {
     @Autowired
     private IUserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signUp(RegisterRequest registerRequest){
 
         User user = new User();
         user.setUserName(registerRequest.getUserName());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
 
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+
+        return passwordEncoder.encode(password);
     }
 
 }
